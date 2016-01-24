8b1f6601e5388766e517618eab4f702a48e9e338
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-01-24 21:50:47 +0000

    Added xunit - one running test

---
+++ src/MvcSite/IntegrationTest/SampleIntegrationTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace MvcSite.IntegrationTest
+{
+	public class SampleIntegrationTest
+    {
+		//Following http://xunit.github.io/docs/getting-started-dnx.html
+
+		[Fact]
+		public void Returns200()
+		{
+			Assert.True(true);
+		}
+    }
+}
