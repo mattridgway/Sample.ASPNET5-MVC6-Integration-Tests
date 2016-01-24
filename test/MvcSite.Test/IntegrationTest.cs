f47cec5236bdac64d2072cf446ef555b4df662cf
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-01-24 22:25:45 +0000

    Moved integration test to separate project, now get error

---
+++ test/MvcSite.Test/IntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.TestHost;
+using System.Net.Http;
+using Xunit;
+
+namespace MvcSite.Test
+{
+	public class IntegrationTest
+    {
+		[Fact]
+		public async void Returns200()
+		{
+			var server = new TestServer(TestServer.CreateBuilder().UseEnvironment("Development").UseStartup<Startup>());
+			var client = server.CreateClient();
+			var request = new HttpRequestMessage(HttpMethod.Get, "/");
+
+			var result = await client.SendAsync(request);
+			Assert.True(result.StatusCode == System.Net.HttpStatusCode.OK);
+		}
+	}
+}
